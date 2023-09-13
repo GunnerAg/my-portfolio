11711502550df72a433709d51727a9ed474c0267
Gunner Andersen Gil <67388056+GunnerAg@users.noreply.github.com>
2023-09-13 14:26:29 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,28 +22,28 @@ export default function Contact() {
                 <p className="contact__details-paragraph--one">You can find links to my networks below, or just send me an email and I would replay as soon as posible!</p>
                 <ul className="contact__details-list">
                     <li className="contact__details-list--element">
-                        <a href="https://wa.me/34698162574">
+                        <a href="https://wa.me/34698162574" target="_blank">
                             <img className="contact__details-logo" src="https://res.cloudinary.com/gunnerag/image/upload/f_auto/v1623576699/whatsapp_fvvncl.png" alt="Whatsapp"/>
                         </a>
                          &nbsp;
                         <div className="contact__details-list--bold">+34 698 16 25 74</div>
                     </li>
                     <li className="contact__details-list--element">
-                        <a href="https://github.com/GunnerAg">
+                        <a href="https://github.com/GunnerAg" target="_blank">
                             <img className="contact__details-logo" src="https://res.cloudinary.com/gunnerag/image/upload/f_auto/v1623576879/GitHub_mukrtc.png" alt="GitHub"/>
                         </a>
                         &nbsp;
                         <div className="contact__details-list--bold">GitHub</div>
                     </li>
                     <li className="contact__details-list--element">
-                        <a href="https://www.linkedin.com/in/gunnerandersengil">
+                        <a href="https://www.linkedin.com/in/gunnerandersengil" target="_blank">
                             <img className="contact__details-logo" src="https://res.cloudinary.com/gunnerag/image/upload/f_auto/v1623576698/LinkedIn_n4qnvz.png" alt="LinkedIn"/>
                         </a>
                         &nbsp;
                         <div className="contact__details-list--bold">LinkedIn</div>
                     </li>
                     <li className="contact__details-list--element">
-                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=@gunner.andersen.gil@gmail.com">
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=@gunner.andersen.gil@gmail.com" target="_blank">
                             <img className="contact__details-logo" src="https://res.cloudinary.com/gunnerag/image/upload/f_auto/v1623576697/gmail_ijo9wd.png" alt="Gmail"/>
                         </a>
                         &nbsp;
